b4f82785853bfe8c4107f544f8103991412aad91
Tamás Kriskó <tamas.krisko@strivacity.com>
2025-11-11 11:07:10 +0100

    demoapplication/...: add designs for new widgets

---
+++ demoapplication/src/main/java/com/strivacity/android/demo/render/widgets/WebauthnLoginWidget.java
@@ -0,0 +1,73 @@
+package com.strivacity.android.demo.render.widgets;
+
+import static com.strivacity.android.demo.render.constants.Dimensions.toPixel;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.strivacity.android.demo.render.constants.Colors;
+import com.strivacity.android.demo.render.constants.Dimensions;
+import com.strivacity.android.native_sdk.render.models.WidgetModel;
+
+import java.util.Objects;
+
+public class WebauthnLoginWidget extends com.strivacity.android.native_sdk.render.widgets.WebauthnLoginWidget {
+
+    public WebauthnLoginWidget(Context context, WidgetModel.WebauthnLoginWidgetModel webauthnLoginWidgetModel) {
+        super(context, webauthnLoginWidgetModel);
+        switch (webauthnLoginWidgetModel.getRender().getType()) {
+            case "button":
+                Button button = this.typedView();
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+
+                params.setMargins(0, 0, 0, Dimensions.toPixel(Dimensions.spaceBetweenButtons));
+                button.setLayoutParams(params);
+
+                GradientDrawable shape = new GradientDrawable();
+                shape.setShape(GradientDrawable.RECTANGLE);
+                shape.setCornerRadius(toPixel(Dimensions.borderRadius));
+
+                String backgroundByVariant;
+                String textByVariant;
+                if (Objects.equals(webauthnLoginWidgetModel.getRender().getHint().getVariant(), "primary")) {
+                    backgroundByVariant = Colors.primary;
+                    textByVariant = Colors.white;
+                } else {
+                    backgroundByVariant = Colors.white;
+                    textByVariant = Colors.primary;
+                }
+
+                shape.setColor(Color.parseColor(backgroundByVariant));
+                button.setBackground(shape);
+                button.setTextColor(Color.parseColor(textByVariant));
+                button.setTextSize(Dimensions.textSizeMedium);
+                button.setAllCaps(false);
+                break;
+            case "link":
+                TextView textView = this.typedView();
+                textView.setLayoutParams(
+                    new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                );
+
+                textView.setPadding(
+                    toPixel(Dimensions.paddingMedium),
+                    toPixel(Dimensions.paddingMedium),
+                    toPixel(Dimensions.paddingMedium),
+                    toPixel(Dimensions.paddingMedium)
+                );
+                textView.setTextSize(Dimensions.textSizeMedium);
+                textView.setTextColor(Color.parseColor(Colors.primary));
+                break;
+        }
+    }
+}
